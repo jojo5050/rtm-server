e67480216c2f08c245a54ceca957a21b97e48fda
joshua <ehiatuejoshua27@gmail.com>
2023-07-13 09:01:01 +0100

    token fix

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const APP_ID = '0c41c495f3794cc1a46ef7656850b9f7';
 const APP_CERTIFICATE = 'ed7c0961341646ecb4aac4c80d48e134';
 const port = 3000;
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+//app.use(function (req, res, next) {
+  //  res.setHeader('Access-Control-Allow-Origin', '*');
+   // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+    //next();
+  //});
 
 app.get('/rtm_token', (req, res) => {
   const userID = req.query.userID;
@@ -31,7 +31,6 @@ app.get('/rtm_token', (req, res) => {
     APP_ID,
     APP_CERTIFICATE,
     userID,
-    RtmRole.Rtm_User,
     privilegeExpiredTs
   );
 
